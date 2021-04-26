e8487e3d6a29f56e2447ac5338c46b1ed3be5a53
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-04-27 01:44:19 +0200

    Add tests/test_producer.py

---
+++ tests/test_producer.py
@@ -0,0 +1,41 @@
+import unittest
+from unittest.mock import patch
+
+from kwebmon.producer.producer import Producer
+
+
+@patch("kwebmon.producer.producer.KafkaProducer")
+class TestProducer(unittest.TestCase):
+    def test_producer_send_message(self, mock_kafka_producer):
+        producer = Producer(
+            "http://test:8000",
+            "test-topic",
+            "cafile",
+            "certfile",
+            "keyfile",
+        )
+        producer.send({"key": "test_key"}, {"value": "test_value"})
+
+        mock_kafka_producer().send.assert_called_with(
+            "test-topic",
+            key={"key": "test_key"},
+            value={"value": "test_value"}
+        )
+        mock_kafka_producer().flush.assert_called_once()
+
+    def test_producer_close(self, mock_kafka_producer):
+        producer = Producer(
+            "http://test:8000",
+            "test-topic",
+            "cafile",
+            "certfile",
+            "keyfile",
+        )
+        producer.close()
+
+        mock_kafka_producer().flush.assert_called_once()
+        mock_kafka_producer().close.assert_called_once()
+
+
+if __name__ == "__main__":
+    unittest.main()
